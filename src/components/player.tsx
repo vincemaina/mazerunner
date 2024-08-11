6113321236188089e22482869bca8a93458f85eb
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-11 15:13:17 +0100

    feat: add player movement

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -21,20 +21,32 @@ export function Player(props: Props) {
             window.addEventListener('keydown', (event) => {
                 switch (event.key) {
                     case 'ArrowUp':
+                    case 'w':
+                    case 'W':
                         console.log('up');
-                        $currentPosition.setKey("y", currentPosition.y - 1);
+                        if ($currentPosition.get().y === 0) return;
+                        $currentPosition.setKey("y", $currentPosition.get().y - 1);
                         break;
                     case 'ArrowDown':
+                    case 's':
+                    case 'S':
                         console.log('down');
-                        $currentPosition.setKey("y", currentPosition.y + 1);
+                        if ($currentPosition.get().y >= props.mazeSize - 1) return;
+                        $currentPosition.setKey("y", $currentPosition.get().y + 1);
                         break;
                     case 'ArrowLeft':
+                    case 'a':
+                    case 'A':
                         console.log('left');
-                        $currentPosition.setKey("x", currentPosition.x - 1);
+                        if ($currentPosition.get().x === 0) return;
+                        $currentPosition.setKey("x", $currentPosition.get().x - 1);
                         break;
                     case 'ArrowRight':
+                    case 'd':
+                    case 'D':
                         console.log('right');
-                        $currentPosition.setKey("x", currentPosition.x + 1);
+                        if ($currentPosition.get().x >= props.mazeSize - 1) return;
+                        $currentPosition.setKey("x", $currentPosition.get().x + 1);
                         break;
                     default:
                         break;
@@ -50,14 +62,17 @@ export function Player(props: Props) {
     }, []);
 
     const playerSize = props.mazeWidth / props.mazeSize;
+    const cellSize = props.mazeWidth / props.mazeSize;
 
     return (
-        <div className="absolute inset-0">
-            <div className="h-full w-full relative">
-                <div className="bg-red-500 aspect-square rounded-full absolute"
-                    style={{top: currentPosition.y / props.mazeSize, left: currentPosition.x / props.mazeSize, width: `${playerSize}px`}}
+        <>
+            <pre className="absolute bottom-0 left-0">{JSON.stringify(currentPosition)}</pre>
+
+            <div className="absolute inset-0">
+                <div className="bg-red-500 aspect-square rounded-full relative w-10"
+                    style={{top: currentPosition.y * cellSize, left: currentPosition.x * cellSize, width: `${playerSize}px`}}
                 />
             </div>
-        </div>
+        </>
     );
 }
