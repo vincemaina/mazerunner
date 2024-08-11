2c14fc5922a3271bd9db17915899e32e09112836
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-11 14:16:54 +0100

    feat: add border + shadow

--- src/components/maze.tsx
+++ src/components/maze.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export function Maze(props: Props) {
     return (
-        <div className="bg-white w-[500px]">
+        <div className="bg-white shadow border-t w-[500px]">
             <Grid
                 size={props.size}
             />
