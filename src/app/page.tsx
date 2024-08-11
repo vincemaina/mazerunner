b031a7a40b15c66d98d2e671d6d34d4590cf4ce7
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-11 14:19:55 +0100

    feat: add width as prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import { Maze } from "@/components/maze";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Pathfinder</h1>
-
-      <Maze size={23}/>
+    <div>
+      <Maze size={23} width={500}/>
     </div>
   );
 }
--- src/components/maze.tsx
+++ src/components/maze.tsx
@@ -1,12 +1,21 @@
+'use client';
+
+import { useEffect } from "react";
 import { Grid } from "./grid";
 
 interface Props {
     size: number;
+    width: number;
 }
 
 export function Maze(props: Props) {
+
+    useEffect(() => {
+
+    }, []);
+
     return (
-        <div className="bg-white shadow border-t w-[500px]">
+        <div className="bg-white shadow border-t" style={{width: props.width}}>
             <Grid
                 size={props.size}
             />
